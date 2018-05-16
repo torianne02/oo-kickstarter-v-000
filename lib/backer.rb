dc975c64ce4cc90e6e5f8b15f3606f6c815f6eef
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 16:56:05 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,9 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)
-    @backed_projects << project
-    new_project = Project.new(project)
+  def back_project(Project)
+    @backed_projects << Project
+    new_project = Project.new(Project)
     new_project.backers << self
   end
 end
